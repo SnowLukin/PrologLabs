7d045e901bf8426e3e031febac7884e35aad670a
Snow Lukin <everyoursden@gmail.com>
2022-04-26 15:57:20 +0300

    Added task 11.11

--- Lab11.pl
+++ Lab11.pl
@@ -11,3 +11,25 @@ woman(mia).
 woman(mary).
 woman(maria).
 woman(mila).
+
+parent(mason, maverick).
+parent(mona, maverick).
+
+parent(michael, maya).
+parent(michael, madison).
+
+parent(mia, maya).
+parent(mia, madison).
+
+parent(matthew, maria).
+parent(mila, maria).
+
+parent(maverick, maddox).
+parent(maya, maddox).
+
+parent(madison, mary).
+parent(maria, mary).
+
+%11.11
+son(X,Y):- parent(Y,X), man(X).
+son(X):- parent(X,Y), man(Y), write(Y), nl.
