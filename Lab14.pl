493662bb72772961b0a585fbd457c4cb6833bcc9
Snow Lukin <everyoursden@gmail.com>
2022-05-28 17:25:43 +0300

    Cosmetic fixes

--- Lab14.pl
+++ Lab14.pl
@@ -1,11 +1,24 @@
-% --------- 1.1 ---------
 writeList([]):- writeln(""), !.
 writeList([Head|Tail]):-
     write(Head), write(" "),
     writeList(Tail).
+
+% --------- 1.1 ---------
+
+% Дана строка. Вывести ее три раза через запятую и показать количество символов в ней.
+/*
+    Example:
+        ?- task1_1(hello).
+        h e l l o
+        Lenght: 5
+        true.
+*/
 
 task1_1(String):-
     atom_chars(String, CharList),
     writeList(CharList),
     length(CharList, Length),
     write("Lenght: "), writeln(Length).
+
+
+
