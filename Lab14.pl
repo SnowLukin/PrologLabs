6200b908899ef88b64b974285b118f273a3d6326
Snow Lukin <everyoursden@gmail.com>
2022-05-28 20:00:32 +0300

    Added task 1.3

--- Lab14.pl
+++ Lab14.pl
@@ -3,6 +3,29 @@ writeList([Head|Tail]):-
     write(Head), write(" "),
     writeList(Tail).
 
+readString(Str,N):-
+    get0(Char),
+    readString(Char, [], 0, Str, N, _).
+
+readString(Str,N, Flag):-
+    get0(Char),
+    readString(Char, [], 0, Str, N, Flag).
+
+readString(-1,Str,N,Str,N,1):-!.
+readString(10,Str,N,Str,N,0):-!.
+
+readString(Char,NowStr,Count,Str,N, Flag):-
+    NewCount is Count+1,
+    appendString(NowStr,[Char],NewNowStr),
+    get0(NewChar),
+    readString(NewChar,NewNowStr,NewCount,Str,N, Flag).
+
+appendString([],X,X).
+appendString([X|T],Y,[X|T1]) :- appendString(T,Y,T1).
+
+writeString([]):-!.
+writeString([H|T]):-put(H),writeString(T).
+
 % --------- 1.1 ---------
 
 % Дана строка. Вывести ее три раза через запятую и показать количество символов в ней.
@@ -20,5 +43,75 @@ task1_1(String):-
     length(CharList, Length),
     write("Lenght: "), writeln(Length).
 
+% --------- 1.2 ---------
 
+% Дана строка. Найти количество слов.
+/*
+    Example:
+        ?- task1_2("somebody once told me").
+        Amount of words: 4
+        true.
+*/
 
+isSpace(Char):-
+    atom_string(Char, NewChar),
+    32 is NewChar, !.
+
+task1_2(String):-
+    atom_chars(String, CharList),
+    include(isSpace, CharList, FilteredList),
+    length(FilteredList, Length),
+    AmountOfWords is Length + 1,
+    write("Amount of words: "), writeln(AmountOfWords).
+
+% --------- 1.3 ---------
+
+% Дана строка, определить самое частое слово
+
+/*
+    Example:
+        ?- task1_3.
+        |: one two one three two one .
+        one
+        true.
+*/
+
+task1_3:-
+    readString(String, _),
+    mostCommonWord(String, MostCommonWord),
+    writeString(MostCommonWord).
+
+countWordString(List, X, Result):-
+    countWordString(List, X, 0, Result).
+countWordString([], _, Result, Result):- !.
+countWordString([X|T], X, Count, Result):-
+    NewCount is Count + 1,
+    countWordString(T, X, NewCount, Result),!.
+countWordString([_|T], X, Count, Result):-
+    countWordString(T, X, Count, Result),!.
+
+splitString([], _, CurWord, CurWordList, Result) :-
+    appendString(CurWordList, [CurWord], NewWL),
+    Result = NewWL,!.
+splitString([Separator|T], Separator, CurWord, CurWordList, Result) :-
+    appendString(CurWordList, [CurWord], NewWL),
+    splitString(T, Separator, [], NewWL, Result),!.
+splitString([S|T], Separator, CurWord, CurWordList, Result) :-
+    appendString(CurWord, [S], NewWord),
+    splitString(T, Separator, NewWord, CurWordList, Result),!.
+splitString(Str, Separator, Result) :-
+    char_code(Separator, SepCode),
+    splitString(Str, SepCode, [], [], Result).
+
+mostCommonWord(Str, Result) :-
+    splitString(Str, " ", Words),
+    mostCommonWord(Words, Words, 0, [], Result).
+mostCommonWord(Words, [Word|T], CurMaxCnt, _, Result) :-
+    countWordString(Words, Word, Cnt),
+    Cnt > CurMaxCnt,
+    NewMax is Cnt,
+    NewMaxWord = Word,
+    mostCommonWord(Words, T, NewMax, NewMaxWord, Result),!.
+mostCommonWord(Words, [_|T], CurMaxCnt, CurMaxWord, Result) :-
+    mostCommonWord(Words, T, CurMaxCnt, CurMaxWord, Result), !.
+mostCommonWord(_, [], _, Result, Result) :-!.
